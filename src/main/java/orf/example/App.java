4c4ace4ce37a959ff801b78ecf13e652814396c2
pb890 <xgazdikp@stuba.sk>
2022-02-21 11:51:32 +0100

    Premenne

--- src/main/java/orf/example/App.java
+++ src/main/java/orf/example/App.java
@@ -2,6 +2,21 @@ package orf.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int celeCislo = 5;
+        long celeVacsieCislo = 5L;
+        float desatinneCislo = 5.5f;
+        double dlhsieDesatinneCislo = 5.5;
+        char znak = 'z';
+        String retazec = "slovo/veta";
+        boolean bool = true ;// = false
+
+        int podiel = 2/3;
+        System.out.println(podiel);
+
+        int zvysok = 2%3;
+        System.out.println(zvysok);
+
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
     }
 }
